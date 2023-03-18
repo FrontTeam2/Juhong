7a63b9bd08c9b331979ab7fbb69932138756e653
dlwnghd <alion0513@naver.com>
2023-03-18 16:56:39 +0900

    :recycle: Refactor : 토큰 불러오는 코드 수정

--- gitapi/src/repository/TokenService.js
+++ gitapi/src/repository/TokenService.js
@@ -4,8 +4,11 @@
 
 const TokenService = {
 	// get (token 가져오기)
-	async getToken() {
-		return `${process.env.REACT_APP_ACCESS_TOKEN}`
+	async getAccessToken() {
+		// Access Token을 반환하는 비동기 함수를 사용합니다.
+		const token = await getTokenAsync() // 예시 코드, getTokenAsync()는 Access Token을 비동기적으로 반환하는 함수입니다.
+		return token
 	},
 }
+
 export default TokenService
